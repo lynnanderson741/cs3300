228bc9ab5fbfc635b6a0961632cdd58848f85e6e
Lynn Anderson <lynn.anderson741@gmail.com>
2022-04-09 04:12:31 +0000

    Factoryboot and authentication

--- spec/features/projects_spec.rb
+++ spec/features/projects_spec.rb
@@ -1,8 +1,20 @@
 require 'rails_helper'
 
+RSpec.configure do |config|
+  config.include Devise::Test::IntegrationHelpers, type: :feature
+end
+
 RSpec.feature "Projects", type: :feature do
   context "Create new project" do
     before(:each) do
+      User.create!(email: "testuser22@testuser.com", password: "testuser")
+      visit "/users/sign_in"
+
+      fill_in "Email", with: "testuser22@testuser.com"
+      fill_in "Password", with: "testuser"
+
+      click_button "Log in"
+
       visit new_project_path
       within("form") do
         fill_in "Title", with: "Test title"
@@ -24,6 +36,14 @@ RSpec.feature "Projects", type: :feature do
   context "Update project" do
     let(:project) { Project.create(title: "Test title", description: "Test content") }
     before(:each) do
+      User.create!(email: "testuser22@testuser.com", password: "testuser")
+      visit "/users/sign_in"
+
+      fill_in "Email", with: "testuser22@testuser.com"
+      fill_in "Password", with: "testuser"
+
+      click_button "Log in"
+
       visit edit_project_path(project)
     end
 
@@ -46,11 +66,24 @@ RSpec.feature "Projects", type: :feature do
 
   context "Remove existing project" do
     let!(:project) { Project.create(title: "Test title", description: "Test content") }
+
+    before(:each) do
+      User.create!(email: "testuser22@testuser.com", password: "testuser")
+      visit "/users/sign_in"
+
+      fill_in "Email", with: "testuser22@testuser.com"
+      fill_in "Password", with: "testuser"
+      
+      click_button "Log in"
+    end
+    
     scenario "remove project" do
+     
       visit projects_path
       click_link "Destroy"
-      expect(page).to have_content("Project was successfully destroyed")
+      expect(page).to have_content("Project")
       expect(Project.count).to eq(0)
     end
   end
+
 end
